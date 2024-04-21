03526555eb9eef15a35102d0e57964108a56dd0a
Kiran Mohan <58102994+KiranM27@users.noreply.github.com>
2024-04-22 00:36:27 +0800

    chore: move the setup file into a scripts folder

---
+++ scripts/requirements.sh
@@ -0,0 +1,16 @@
+echo "Installing Dependencies"
+
+echo "Installing Tree"
+brew install tree
+
+echo "Installing Zoxide"
+brew install zoxide
+
+echo "Installing Stow"
+brew install stow
+
+echo "Installing Tmux"
+brew install tmux
+
+echo "Installing the Tmux Package Manager (TPM)"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
